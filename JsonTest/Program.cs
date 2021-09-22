84660e533e1190becd1db764be4bd0749aae14e7
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-09-22 11:25:32 +0700

    Add project files.

---
+++ JsonTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JsonTest
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main()
+        {
+            var user = new User
+            {
+                Name = "Andrey",
+                Age = 33
+            };
+
+            using (var file_output = new FileStream("user.json", FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(file_output, user);
+            }
+
+            var person = new User();
+            using (var file_input = new FileStream("user.json", FileMode.Open))
+            {
+                person = JsonSerializer.DeserializeAsync<User>(file_input).Result;
+            }
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
+
